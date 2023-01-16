2bc9f5bd9850bd8a6df5dc6dc739d02e5441d918
Asia K <66960776+asiakay@users.noreply.github.com>
2023-01-15 21:30:34 -0500

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'EFAD',
+    site_url: 'https://efad.vercel.app/',
+    feed_url: 'https://efad.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
