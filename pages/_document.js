5d7cfc796837c37e4f33a8b3b152ee7ad05897d0
Asia K <66960776+asiakay@users.noreply.github.com>
2023-01-15 21:31:58 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'EFAD',
+    description: 'EdTech | Forensics | Art | DevOps',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
